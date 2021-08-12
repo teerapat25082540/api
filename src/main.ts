aaf885556d63702abfea436a1fc686a37b88a225
pirada-k039 <pirada.k0627@hotmail.com>
2021-08-12 17:31:10 +0700

    edit swagger title

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Crud')
+    .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
